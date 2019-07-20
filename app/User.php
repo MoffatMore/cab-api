d1b90cbd3bb40b0d45293dc5518704865bb5d05e
MoffatMore <mofenyimoffat@gmail.com>
2019-07-20 18:29:17 +0200

    fixing models

--- app/User.php
+++ app/User.php
@@ -37,8 +37,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function bookings(){
-
-        return $this->hasMany('App\Booking');
-    }
+//    public function bookings(){
+//
+//        return $this->hasMany('App\Booking');
+//    }
 }
