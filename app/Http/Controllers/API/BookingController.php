66ba00080b5f627941bf4b963eec6a1ef794f69f
MoffatMore <mofenyimoffat@gmail.com>
2019-07-20 18:57:25 +0200

    fixing models

--- app/Http/Controllers/API/BookingController.php
+++ app/Http/Controllers/API/BookingController.php
@@ -65,7 +65,7 @@ class BookingController extends Controller
 
         foreach ($bookings as $booking){
             $user = User::find($booking->user_id);
-            $booking['user_id'] = json(['user'=>$user]);
+            $booking['user_id'] = $user;
         }
 
         return $bookings;
